a2595d27fbc4a6f94e942033ab6b0299efac1257
rodi38 <rodrigorochaed1@gmail.com>
2023-06-25 05:11:03 -0300

    adding a few tests with json

--- src/app/components/VideoComponent/VideoComponent.tsx
+++ src/app/components/VideoComponent/VideoComponent.tsx
@@ -1,12 +1,8 @@
 import React from "react";
+import Image from "next/image";
 
 async function VideoComponent() {
-  const res = await fetch("http://localhost:3004/videos");
-  if (!res.ok) {
-    throw new Error('failed to fetch');
-  }
-
-  return <div>VideoComponent</div>;
+  
+  return <div></div>
 }
-
 export default VideoComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,42 @@ import styles from "./page.module.css";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import HeroComponent from "./components/HeroComponent/HeroComponent";
 import BlogComponent from "./components/BlogComponent/BlogComponent";
-export default function Home() {
+
+async function getPublicationData() {
+  const res = await fetch(`http://localhost:3004/videos`);
+  if (!res.ok) {
+    throw new Error("failed to fetch");
+  }
+
+  return res.json();
+}
+// async function getVideosData(arr: Array<string>, loopNumber: number) {
+//   arr.map((item) =>{
+//     return (
+//     <p key={item}>
+//       {item}
+//     </p>
+//     )
+//   })
+
+// }
+export default async function Home() {
+  const data = await getPublicationData();
+  const data2 = await Promise.all(data);
+
   return (
     <main>
       <HeaderComponent></HeaderComponent>
       <HeroComponent></HeroComponent>
       <BlogComponent></BlogComponent>
+      <ul>
+        {data2.map((item) => (
+          <li key={item.id}>
+            <p>{item.descripgittion}</p>
+            <a href="#">{item.link}</a>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
